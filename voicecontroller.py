56d4207f892203f58aa81b09c06fa0a2399a0448
seajon16 <seajon16@vt.edu>
2020-11-08 17:44:07 -0500

    Remove unused import

--- voicecontroller.py
+++ voicecontroller.py
@@ -8,7 +8,7 @@ from gtts import gTTS
 # _extra_langs is needed for now since there's a bug in gTTS
 from gtts.lang import tts_langs, _extra_langs
 from datetime import datetime
-from functools import wraps, partial
+from functools import partial
 import logging
 import editdistance
 
